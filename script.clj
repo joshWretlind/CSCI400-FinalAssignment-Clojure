d2163a74ea826f567b0b865a4e3bb0347c39b565
Nick Wunder <Nick.Wunder@nrel.gov>
2014-04-28 16:04:02 -0600

    multiply rows and cols

--- script.clj
+++ script.clj
@@ -1,1 +1,34 @@
-(javax.swing.JOptionPane/showMessageDialog nil "Hello World")
+(def dim 3)
+(def r #(rand-int %))
+
+(def m (into [] (repeat row (into [] (repeat col (atom []))))))
+
+
+(def a
+     (apply vector
+            (map (fn [_]
+                   (apply vector (map (fn [_] (rand-int 10))
+                                      (range dim))))
+                 (range dim))))
+
+(def b
+    (apply vector
+           (map (fn [_]
+                  (apply vector (map (fn [_] (rand-int 10))
+                                     (range dim))))
+                (range dim))))
+
+(def soln
+     (apply vector
+            (map (fn [_]
+                   (apply vector (map (fn [_] (agent (0))
+                                      (range dim))))
+                 (range dim))))
+
+(defn mult-rc [ai bi] (reduce + (map * (nth a ai) (nth b bi))))
+
+
+
+(defn columnize [i] (for [n b] (nth n i)))
+
+(into [] (for [i (range dim)] (into [] (columnize i))))
