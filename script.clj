d3944f52866f847a3c32106bbfd81ed291bcf195
Nick Wunder <Nick.Wunder@nrel.gov>
2014-04-29 09:10:16 -0600

    can do matrix multiplication

--- script.clj
+++ script.clj
@@ -1,34 +1,72 @@
-(def dim 3)
+(def dim 512)
 (def r #(rand-int %))
 
-(def m (into [] (repeat row (into [] (repeat col (atom []))))))
-
+;(def m (into [] (repeat row (into [] (repeat col (atom []))))))
 
 (def a
      (apply vector
             (map (fn [_]
-                   (apply vector (map (fn [_] (rand-int 10))
+                   (apply vector (map (fn [_] (rand 10))
                                       (range dim))))
                  (range dim))))
 
 (def b
     (apply vector
            (map (fn [_]
-                  (apply vector (map (fn [_] (rand-int 10))
+                  (apply vector (map (fn [_] (rand 10))
                                      (range dim))))
                 (range dim))))
 
 (def soln
      (apply vector
             (map (fn [_]
-                   (apply vector (map (fn [_] (agent (0))
+                   (apply vector (map (fn [_] (agent 0))
                                       (range dim))))
                  (range dim))))
 
+(defn print-matrix [m] (dotimes [i dim] (print (nth m i) "\n")))
+(defn print-soln-matrix [m] (dotimes [i dim] (print (nth m i) "\n")))
+
+;(print-matrix a)
+(print "\n")
+;(print-matrix b)
+(print "\n")
+;(print-matrix soln)
+(print "\n")
+
 (defn mult-rc [ai bi] (reduce + (map * (nth a ai) (nth b bi))))
 
+(defn get-elem [ai bi] (nth (nth soln ai) bi))
+
+(defn mult-matrix [] (dotimes [i dim] (dotimes [j dim] (cond (and i j) (send (get-elem i j) + (mult-rc i j))))))
+
+;(defn update [] nil)
+;(defn update [ai bi] (print ai bi))
+
+(defn show-mult [] (for [i (range dim)] (for [j (range dim)] (print (get-elem i j) (mult-rc i j)))))
+
+(time (mult-matrix))
+(time (mult-matrix))
+(time (mult-matrix))
+(time (mult-matrix))
+(time (mult-matrix))
+(time (mult-matrix))
+(time (mult-matrix))
+(time (mult-matrix))
+(time (mult-matrix))
+(time (mult-matrix))
+(time (mult-matrix))
+(time (mult-matrix))
+(time (mult-matrix))
+(time (mult-matrix))
+(time (mult-matrix))
+(time (mult-matrix))
+
+;(print-matrix soln)
+(print "\n")
+;(send (get-elem 0 0) + (mult-rc 0 0))
 
 
-(defn columnize [i] (for [n b] (nth n i)))
+;(defn columnize [i] (for [n b] (nth n i)))
 
-(into [] (for [i (range dim)] (into [] (columnize i))))
+;(into [] (for [i (range dim)] (into [] (columnize i))))
